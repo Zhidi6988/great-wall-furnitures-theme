94fd6926f03d944f2a4802998facffbb188b347d
Zhidi6988 <zhidihuang69@gmail.com>
2026-06-11 11:12:09 +0400

    Add files via upload

--- page-portfolio.php
+++ page-portfolio.php
@@ -5,7 +5,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Portfolio - 3D Scroll Diagnostic</title>
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/styles.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/portfolio.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
